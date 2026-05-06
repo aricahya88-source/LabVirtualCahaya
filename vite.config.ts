7836c5fc12ac8760918f11668c18948b273f7cef
aricahya88-source <ari.cahya88@gmail.com>
2026-05-07 00:19:49 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/wave-ar-virtual-labV3/",
+  base: "/LabVirtualCahaya/",
   server: {
     host: "0.0.0.0",
     port: 5173
